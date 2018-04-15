a899021c51b44719285a55df5a64be3ddfbf8ee7
PatrickTopor <35204198+PatrickTopor@users.noreply.github.com>
2018-04-15 16:42:01 -0700

    Update gameflowAndInteractions.js

--- 3PlayerDiceGame/gameflowAndInteractions.js
+++ 3PlayerDiceGame/gameflowAndInteractions.js
@@ -1,8 +1,5 @@
 var gameflow = {
-    
-    
-    
-    
-    
-    
-}
+    player1Lost: "You already lost, " +player1.name+ ".  But you should stick around to see who will win!",
+    player2Lost: "You already lost, " +player2.name+ ".  But you should stick around to see who will win!",
+    player3Lost:"You already lost, " +player3.name+ ".  But you should stick around to see who will win!"
+    };
