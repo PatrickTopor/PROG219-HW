7c84f2ea0e37f7a53ad9d50b807970cef440542b
PatrickTopor <35204198+PatrickTopor@users.noreply.github.com>
2018-04-15 16:52:43 -0700

    Update gameflowAndInteractions.js

--- 3PlayerDiceGame/gameflowAndInteractions.js
+++ 3PlayerDiceGame/gameflowAndInteractions.js
@@ -1,5 +1,18 @@
 var gameflow = {
     player1Lost: "You already lost, " +player1.name+ ".  But you should stick around to see who will win!",
     player2Lost: "You already lost, " +player2.name+ ".  But you should stick around to see who will win!",
-    player3Lost:"You already lost, " +player3.name+ ".  But you should stick around to see who will win!"
+    player3Lost:"You already lost, " +player3.name+ ".  But you should stick around to see who will win!",
+    player1LostRound:"You lost this round: "+ player1.name  +"!  Try again and you might win!",
+    player2LostRound:"You lost this round: "+ player2.name  +"!  Try again and you might win!",
+    player3LostRound:"You lost this round: "+ player3.name  +"!  Try again and you might win!",
+    player1gameOver:"GAME OVER FOR: " + player1.name+"!",
+    player2gameOver:"GAME OVER FOR: " + player2.name+"!",
+    player3gameOver:"GAME OVER FOR: " + player3.name+"!",
+    player1WonRound:"You won: "+player1.name+"!  Play again to win more!",
+    player2WonRound:"You won: "+player2.name+"!  Play again to win more!",
+    player3WonRound:"You won: "+player3.name+"!  Play again to win more!",
+    player1WonGame:"The winner is: " + player3.name,
+    player2WonGame:"The winner is: " + player3.name,
+    player3WonGame:"The winner is: " + player3.name,
+    
     };
