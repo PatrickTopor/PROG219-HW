40f5d7fcf1e4f418f62a343f335df1c906b3fd82
PatrickTopor <35204198+PatrickTopor@users.noreply.github.com>
2018-04-16 06:55:55 -0700

    Update game.js

--- 3PlayerDiceGame/game.js
+++ 3PlayerDiceGame/game.js
@@ -23,23 +23,6 @@
         }  
     };
     
-        let interactions = {
-        
-  player1Winner: "The winner is: " + player1.name,
-  player2Winner: "The winner is: " + player2.name,
-  player3Winner: "The winner is: " + player3.name,
-player1Won: "You won: "+player1.name+"!  Play again to win more!",
-player2Won: "You won: "+player2.name+"!  Play again to win more!",
-player3Won: "You won: "+player3.name+"!  Play again to win more!",
-player1Done: "GAME OVER FOR: " +player1.name+ "!",
-player2Done: "GAME OVER FOR: " +player2.name+ "!",
-player3Done: "GAME OVER FOR: " +player3.name+ "!",
-player1LostRound: "You lost this round: "+ player1.name  +"!  Try again and you might win!",
-player2LostRound: "You lost this round: "+ player1.name  +"!  Try again and you might win!",
-player3LostRound: "You lost this round: "+ player1.name  +"!  Try again and you might win!",
-    
-};
-    
     if (player1.IsPlayerBroke == true && player2.IsPlayerBroke == true){
         $("#status").text (interactions.player3Winner);
         $("#ButtonBet").hide();
