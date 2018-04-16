282a43c7b0e6880591d4c070ddbef3783f93cbd3
PatrickTopor <35204198+PatrickTopor@users.noreply.github.com>
2018-04-16 06:56:16 -0700

    Update gameflowAndInteractions.js

--- 3PlayerDiceGame/gameflowAndInteractions.js
+++ 3PlayerDiceGame/gameflowAndInteractions.js
@@ -1,18 +1,16 @@
-var gameflow = {
-    player1Lost: "You already lost, " +player1.name+ ".  But you should stick around to see who will win!",
-    player2Lost: "You already lost, " +player2.name+ ".  But you should stick around to see who will win!",
-    player3Lost:"You already lost, " +player3.name+ ".  But you should stick around to see who will win!",
-    player1LostRound:"You lost this round: "+ player1.name  +"!  Try again and you might win!",
-    player2LostRound:"You lost this round: "+ player2.name  +"!  Try again and you might win!",
-    player3LostRound:"You lost this round: "+ player3.name  +"!  Try again and you might win!",
-    player1gameOver:"GAME OVER FOR: " + player1.name+"!",
-    player2gameOver:"GAME OVER FOR: " + player2.name+"!",
-    player3gameOver:"GAME OVER FOR: " + player3.name+"!",
-    player1WonRound:"You won: "+player1.name+"!  Play again to win more!",
-    player2WonRound:"You won: "+player2.name+"!  Play again to win more!",
-    player3WonRound:"You won: "+player3.name+"!  Play again to win more!",
-    player1WonGame:"The winner is: " + player3.name,
-    player2WonGame:"The winner is: " + player3.name,
-    player3WonGame:"The winner is: " + player3.name,
+        let interactions = {
+        
+  player1Winner: "The winner is: " + player1.name,
+  player2Winner: "The winner is: " + player2.name,
+  player3Winner: "The winner is: " + player3.name,
+player1Won: "You won: "+player1.name+"!  Play again to win more!",
+player2Won: "You won: "+player2.name+"!  Play again to win more!",
+player3Won: "You won: "+player3.name+"!  Play again to win more!",
+player1Done: "GAME OVER FOR: " +player1.name+ "!",
+player2Done: "GAME OVER FOR: " +player2.name+ "!",
+player3Done: "GAME OVER FOR: " +player3.name+ "!",
+player1LostRound: "You lost this round: "+ player1.name  +"!  Try again and you might win!",
+player2LostRound: "You lost this round: "+ player1.name  +"!  Try again and you might win!",
+player3LostRound: "You lost this round: "+ player1.name  +"!  Try again and you might win!",
     
-    };
+};
