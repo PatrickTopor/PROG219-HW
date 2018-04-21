da42f49eda87f0b00d125459c0e5c54c9a90725c
PatrickTopor <35204198+PatrickTopor@users.noreply.github.com>
2018-04-20 20:06:56 -0700

    Update components.js

--- RainGame/src/components.js
+++ RainGame/src/components.js
@@ -9,11 +9,11 @@ Crafty.c('Drop',{
 });
 
 //this is supposed to load the sprite
-Crafty.load("raindrop.png", function(){
+
 Crafty.sprite("raindrop.png",{
     spr_drop:[0,0, 20, 30],
     });
-    });
+
 
 
 
