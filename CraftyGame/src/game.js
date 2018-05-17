1c6dd020e6cc6442c2f14de0f3fa157725a48823
PatrickTopor <35204198+PatrickTopor@users.noreply.github.com>
2018-05-17 16:23:18 -0700

    Update game.js
    Added function that calls the opening scene

--- CraftyGame/src/game.js
+++ CraftyGame/src/game.js
@@ -1,1 +1,15 @@
 //This is the javascript file for the game logic
+let screenWidth = 1200;
+let screenHeight = 800;
+
+//Crafty.init(screenWidth,screenHeight, document.getElementById('game'));
+
+// Initialize and start our game
+start:function() {
+  // Start crafty and set a background color so that we can see it's working  
+  Crafty.init(screenWidth, screenHeight);  
+  Crafty.background('rgb(193, 104, 202)');  
+  // Simply start the "Game" scene to get things going  
+  Crafty.scene('Opening1');  
+  }
+}
