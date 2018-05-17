23a024371f5670dd0df48aecf114c86fb319a3a2
PatrickTopor <35204198+PatrickTopor@users.noreply.github.com>
2018-05-17 16:20:05 -0700

    Create scenes.js

---
+++ CraftyGame/src/scenes.js
@@ -0,0 +1,36 @@
+Crafty.scene('Opening1', function(){
+//this will open the game and explain the story to the player
+
+});
+
+Crafty.scene('Game1', function(){
+//this is the first game, where the explorer is trying to enter the temple
+//with a hail of knives trying to hit them
+//if they die, the game restarts
+
+});
+
+Crafty.scene('Opening2', function(){
+//after the player enters the temple, they are told to find the tresure 
+//that will ensutre that they will make it our alive
+
+});
+
+Crafty.scene('Game2', function(){
+//this is the second game, where the player looks around and tries to find the tresure
+//before the natives find and kill them
+//must add a timer here
+
+});
+
+Crafty.scene('Victory', function(){
+//if the player does not find the tresure, they loose and get killed
+//add button for them to retry the 2nd game
+
+});
+
+Crafty.scene('Loss', function(){
+//if the player finds the treasure, they win and give a long lost treasure to the natives,
+//who are gratful enought to let the player leave with the remaining treasure alive
+
+});
