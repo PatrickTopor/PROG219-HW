8a5b77df6c1efc543a32282badc9ddb5b229abd8
PatrickTopor <35204198+PatrickTopor@users.noreply.github.com>
2018-05-21 18:04:54 -0700

    Update game.js

--- CraftyGame/src/game.js
+++ CraftyGame/src/game.js
@@ -5,11 +5,11 @@ let screenHeight = 800;
 //Crafty.init(screenWidth,screenHeight, document.getElementById('game'));
 
 // Initialize and start our game
-start:function() {
+//start:function() {
   // Start crafty and set a background color so that we can see it's working  
   Crafty.init(screenWidth, screenHeight, document.getElementById('game'));  
   Crafty.background('rgb(193, 104, 202)');  
   // Simply start the "Game" scene to get things going  
   Crafty.scene('Opening1');  
-  }
-}
+  //}
+//}
