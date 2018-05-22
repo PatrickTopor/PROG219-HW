a18e899438923c23eed27a197ce1dcb683009c1e
PatrickTopor <35204198+PatrickTopor@users.noreply.github.com>
2018-05-21 18:11:44 -0700

    Update game.js

--- CraftyGame/src/game.js
+++ CraftyGame/src/game.js
@@ -1,17 +1,72 @@
-//This is the javascript file for the game logic
-let screenWidth = 1200;
-let screenHeight = 800;
+//Setting screen size and hit counter
+let screenWidth = 800;
+let screenHeight = 400;
+let hitCounter = 0;
+//init game
+Crafty.init(screenWidth,screenHeight, document.getElementById('game'));
+//add floor
+Crafty.e('Floor')
+.attr({x: 0, y: screenHeight - 20});//floor location
+//add screen border
+Crafty.e('ScreenSide')
+.attr({x:0,y:0});
+//add player
+let player1 = Crafty.e('Player')
+.attr({x: 20, y: 200})//player location
+.bind("EnterFrame", function(){
+  if (this.x == screenWidth)
+  {
+    pause();
+    Crafty.e('2D, DOM, Text')
+    .attr({x:screenWidth/2, y:screenHeight/2})
+    .text("Stage 1 Clear")
+    .textFont({size:'20px', weight:'bold'})
+    .textColor('#FFFFFF');
+  }
+});
+//add hit text
+let hitText = Crafty.e('2D,DOM, Text')
+.attr({//hitText location
+  x: screenWidth - 100,
+  y: 10
+});
+//display text
+hitText.text('Hit:' + hitCounter);
+//set text style
+hitText.textFont({
+  size: '30px',
+  weight: 'bold'
+})
+.textColor('#FFFFFF');
+// drop rain at random x place from 50 to 50+screenWidth
+function drop()
+{
+  let randomx = Math.floor((Math.random() * screenWidth) + 60);
+    Crafty.e('Drop')
+        .attr({x: randomx, y: 0, w: 2, h: 10})
+        .onHit('Player', function(){
+            this.destroy();
+            hitCounter++;
+            hitText.text("Hit:" + hitCounter);
 
-//Crafty.init(screenWidth,screenHeight, document.getElementById('game'));
-
-// Initialize and start our game
-//start:function() {
-  // Start crafty and set a background color so that we can see it's working  
-
-  Crafty.init(screenWidth, screenHeight, document.getElementById('game'));  
-  Crafty.background(red);  
-
-  // Simply start the "Game" scene to get things going  
-  //Crafty.scene('Opening1');  
-  //}
-//}
+            if (hitCounter == 5)//reset player1 location and hit counter
+            {
+              player1.x = 20;
+              hitCounter = 0;
+              hitText.text("Hit:" + hitCounter);
+            }
+        })
+        .bind("EnterFrame", function() {
+            if (this.y > screenHeight-20)
+              this.destroy();
+        });
+}
+function pause()
+{
+  Crafty.pause();
+}
+Crafty.bind("EnterFrame", function(){
+  document.getElementById("message").innerHTML = Crafty.frame();
+  if (Crafty.frame() % 4 == 0)
+    drop();
+});
