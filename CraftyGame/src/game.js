6bc065d5e7346da39bd95f4e109abb25696473e0
PatrickTopor <35204198+PatrickTopor@users.noreply.github.com>
2018-05-30 18:28:13 -0700

    Update game.js

--- CraftyGame/src/game.js
+++ CraftyGame/src/game.js
@@ -1,16 +1,14 @@
 Game = {
 
-  map_grid: {
-    width: 24,
-    height: 16,
-    tile: {
-      width: 16,
-      height: 16
-    }
-    },
-start: function() {
-  // Start crafty and set a background color so that we can see it's working
-    Crafty.init(Game.width(), Game.height());
-    Crafty.background('rgb(249, 223, 125)');
-}
+    start: function() {
 
+        Crafty.init(1200, 800);
+        Crafty.background('rgb(206, 215, 183)');
+        Crafty.scene('opening1');
+
+        //starts the game function after a few second(s)
+        Crafty.e("Delay").delay(function () {
+            Crafty.scene('Game1');
+        }, 4000, 0);
+    }//end function
+}; //end Game
