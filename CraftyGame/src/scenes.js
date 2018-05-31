27fa412bbc2087afadf9a8fe048aa6c8a2c52f45
PatrickTopor <35204198+PatrickTopor@users.noreply.github.com>
2018-05-30 18:27:04 -0700

    Update scenes.js

--- CraftyGame/src/scenes.js
+++ CraftyGame/src/scenes.js
@@ -1,63 +1,254 @@
-Crafty.scene('Opening1', function(){
-//this will open the game and explain the story to the player
 
-});
+
+Crafty.scene('opening1', function(){
+
+    //somehow cannot get the game to work, must figure out later
+    /*var sound = {
+        "audio":{
+            "first":["../sounds/jungleMusic.mp3"]
+        }
+
+    }
+
+    Crafty.load({sound})
+
+    Crafty.audio.play("first");*/
+
+    //Crafty.audio.add("first", "../sounds/jungleMusic.mp3");
+    //Crafty.audio.play("first");
+
+    var ent = Crafty.e("2D, DOM, Image")
+    .attr({w: 1200, h: 800})
+    .image("images/jungle3.jpg");
+
+    Crafty.e('2D, DOM, Text')
+    .attr({x: 500, y: 100, w: 200})
+    .text("Welcome to the Temple Adventure Game!")
+    .textColor('white');
+
+    Crafty.e('2D, DOM, Text')
+    .text("You are a professor of History at the prestigious Bellevue College, and you have heard of a lost temple hidden in the jungles of Val Verde.")
+    .attr({x: 300, y: 125, w: 650})
+    .textColor('white');
+
+    Crafty.e('2D, DOM, Text')
+    .text("You boarded a plane and traveled to this country to see if the rumors had some truth to it, and after a long donkey ride, you see the ruins of the temple.")
+    .attr({x: 250, y: 150, w: 700})
+    .textColor('white');
+
+    Crafty.e('2D, DOM, Text')
+    .text("But what would an adventure be without a little danger, you thought as you approach the temple.  As you step closer, your wish is granted.")
+    .attr({x: 300, y: 175, w: 700})
+    .textColor('white');
+
+    Crafty.e('2D, DOM, Text')
+    .text("A large group of natives spring from the top of the temple and start shouting at you.  As you try to think what to say to them, a knife barely misses you.")
+    .attr({x: 275, y: 200, w: 700})
+    .textColor('white');
+
+    Crafty.e('2D, DOM, Text')
+    .text("The temple entrence is unguared and looks to be your only place of refuge at the moment, and as the flurry of knifes start to come at you, you must reach it as soon as possible.")
+    .attr({x: 250, y: 225, w: 800})
+    .textColor('white');
+
+    Crafty.e('2D, DOM, Text')
+    .text("INSTRUCTIONS:")
+    .attr({x: 500, y: 350, w: 200})
+    .textColor('white');
+
+    Crafty.e('2D, DOM, Text')
+    .text("Use the left and right arrow keys on the keyboard to move side to side, and the up arrow key to jump.")
+    .attr({x: 400, y: 375, w: 500})
+    .textColor('white');
+
+    Crafty.e('2D, DOM, Text')
+    .text("Jump from block to block to reach the upper levels of the game.")
+    .attr({x: 400, y: 400, w: 500})
+    .textColor('white');
+
+    Crafty.e('2D, DOM, Text')
+    .text("Get to the other side and through the temple entrance before your charecter gets stabbed 5 times.")
+    .attr({x: 400, y: 425, w: 500})
+    .textColor('white');
+
+    Crafty.e('2D, DOM, Text')
+    .text("If you die, the game will restart and continue until you win.")
+    .attr({x: 400, y: 450, w: 500})
+    .textColor('white');
+
+});//end opening1 scene
 
 Crafty.scene('Game1', function(){
-//this is the first game, where the explorer is trying to enter the temple
-//with a hail of knives trying to hit them
-//if they die, the game restarts
-    var btn1 = document.getElementByID('StartFirstGame');
-    btn1.style.display = "none";
+    let screenWidth = 1200;
+    let screenHeight = 800;
+    let hitCounter = 0;
+
+    //add floor
+    Crafty.e('Floor')
+    .attr({x: 0, y: 780});
+
+    Crafty.e('ScreenSide')
+    .attr({x:0,y:0});
+    //screens to trap player in game
+    Crafty.e('ScreenSide2')
+    .attr({x:1190,y:100});
+
+    Crafty.e('block')
+    .attr({x:0, y: 680});
+
+    Crafty.e('block')
+    .attr({x:590, y: 680});
+
+    Crafty.e('tree')
+    .attr({x: 780, y: 650});
+
+    Crafty.e('tree2')
+    .attr({x: 550, y: 650});
+
+    Crafty.e('tree')
+    .attr({x: 450, y: 650});
+
+    Crafty.e('tree2')
+    .attr({x: 250, y: 650});
+
+    Crafty.e('tree')
+    .attr({x: 150, y: 650});
+
+    Crafty.e('tree')
+    .attr({x: 50, y: 650});
+
+    Crafty.e('tree2')
+    .attr({x: 875, y: 650});
+
+    Crafty.e('tree')
+    .attr({x: 1075, y: 650});
+
+        //blocks ahead
+        Crafty.e('Block, 2D, Canvas, Color, Floor')
+        .attr({x: 100, y:500, w: 30, h:30})
+        .color('grey');
+    
+        Crafty.e('Block, 2D, Canvas, Color, Floor')
+        .attr({x: 600, y:600, w: 30, h:30})
+        .color('grey');
+    
+        Crafty.e('Block, 2D, Canvas, Color, Floor')
+        .attr({x: 300, y:600, w: 60, h:30})
+        .color('grey');
+    
+        Crafty.e('Block, 2D, Canvas, Color, Floor')
+        .attr({x: 900, y:400, w: 30, h:30})
+        .color('grey');
+    
+        Crafty.e('Block, 2D, Canvas, Color, Floor')
+        .attr({x: 1000, y:375, w: 60, h:30})
+        .color('#A9A9A9');
+    
+        Crafty.e('Block, 2D, Canvas, Color, Floor')
+        .attr({x: 600, y:400, w: 60, h:30})
+        .color('grey');
+    
+        Crafty.e('Block, 2D, Canvas, Color, Floor')
+        .attr({x: 770, y:225, w: 60, h:30})
+        .color('grey');
+    
+        Crafty.e('Block, 2D, Canvas, Color, Floor')
+        .attr({x: 500, y:175, w: 30, h:30})
+        .color('grey');
+    
+        Crafty.e('Block, 2D, Canvas, Color, Floor')
+        .attr({x: 220, y:260, w: 60, h:30})
+        .color('grey');
+    
+        Crafty.e('Block, 2D, Canvas, Color, Floor')
+        .attr({x: 1160, y:100, w: 40, h:50})
+        .color('black');
+
     let player1 = Crafty.e('Player')
     .attr({x: 20, y: 200})//player location
+
+    //now just call the enxt scene, but on a 3-5 second delay
     .bind("EnterFrame", function(){
-    if (this.x == screenWidth)
+        if (this.x == 1200)
+        {
+            
+            Crafty.scene('opening2');
+            //call the second game, but make it delayed like the first one
+        }
+      });
+
+      //shows the player how many times they've been hit
+      let hitText = Crafty.e('2D,DOM, Text')
+      .attr({
+        x: screenWidth - 100,
+        y: 10
+      });
+
+      //rain function
+      function drop()
       {
-        Crafty.scene('opening2');
-          var btn2 = document.getElementByID('StartSecondGame');
-          //now shows the button for the second game with the second opening scene
-          btn2.style.display = "block";
+        let randomx = Math.floor((Math.random() * screenWidth) + 60);
+          Crafty.e('Drop')
+              .attr({x: randomx, y: 0, w: 2, h: 10})
+              .onHit('Player', function(){
+                  this.destroy();
+                  hitCounter++;
+                  hitText.text("Hit:" + hitCounter);
+      
+                  if (hitCounter == 5)//reset player location and hit counter
+                  {
+                    player1.x = 20;
+                    hitCounter = 0;
+                    hitText.text("Hit:" + hitCounter);
+                  }
+              })
+              .bind("EnterFrame", function() {
+                  if (this.y > screenHeight-20)
+                    this.destroy();
+              });
       }
-    });
 
-});
+      //controls the rate of rain
+      Crafty.bind("EnterFrame", function(){
+        if (Crafty.frame() % 10 == 0)
+        {
+          drop();
+        }
+      });
 
-Crafty.scene('Opening2', function(){
-//after the player enters the temple, they are told to find the tresure 
-//that will ensutre that they will make it our alive
+});//end Game1 scene
 
-});
+Crafty.scene('opening2', function() { 
 
-Crafty.scene('Game2', function(){
-//this is the second game, where the player looks around and tries to find the tresure
-//before the natives find and kill them
-//must add a timer here
-  var btn2 = document.getElementByID('StartSecondGame');
-  btn2.style.display = "none";
-  let player1 = Crafty.e('Player')
-    .attr({x: 20, y: 200})//player location
-    .bind("EnterFrame", function(){
-    if (//the player finds the treasure && the time has not run out)
-      {
+    Crafty.e('2D, DOM, Text')
+    .text("Congrats!  You made it to the temple alive!  ")
+    .attr({x: 500, y: 100, w: 200});
+
+    //add the instructions for the player here
+
+});//end opening2 scene
+
+Crafty.scene('Game2', function() { 
+    //this is where Taofei's game goes
+    /*
+    if(all the puzzles are solved)
+    {
         Crafty.scene('Victory');
-      }
-     if(//the player runs out of time)
-       {
-         Crafty.scene('Loss');
-       }
-    });
+    }
+    else
+    {
+        Crafty.scene('Defeat');
+    }
+    */
 
 });
 
-Crafty.scene('Victory', function(){
-//if the player does not find the tresure, they loose and get killed
-//add button for them to retry the 2nd game
+Crafty.scene('Victory', function(){ 
 
+    //congragulate the player on winning
 });
 
-Crafty.scene('Loss', function(){
-//if the player finds the treasure, they win and give a long lost treasure to the natives,
-//who are gratful enought to let the player leave with the remaining treasure alive
+Crafty.scene('Defeat', function(){
 
+//tell the player they got killed by the natives
 });
