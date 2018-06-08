cbf2a7522ddf68f8bd0faaacb49e5e330edf1284
PatrickTopor <35204198+PatrickTopor@users.noreply.github.com>
2018-06-08 08:49:52 -0700

    Update components.js

--- craftyGame_merged/src/components.js
+++ craftyGame_merged/src/components.js
@@ -117,7 +117,7 @@ Crafty.c('ScreenSide2',{
 Crafty.c('Floor',{
     init:function(){
         this.requires('2D, Canvas, Solid, Color')
-        .attr({w: 1200, h: 10})
+        .attr({w: 1200, h: 20})
         .color('#29AB46');
     }
 });
